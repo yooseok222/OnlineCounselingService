1e610df6bc6624c0d24c30b8e1b0393f088c836c
htakimsean <dustlrka86@gmail.com>
2025-05-13 16:18:53 +0900

    git init

---
+++ src/test/java/kr/or/kosa/visang/VisangApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.kosa.visang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
