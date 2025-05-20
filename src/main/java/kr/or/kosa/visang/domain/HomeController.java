cc83eae09498a867c98dc72199878f0add6b498b
won0845 <tpqms775@gmail.com>
2025-05-20 12:30:13 +0900

    feat : adminSidebar 다국어 처리

--- src/main/java/kr/or/kosa/visang/domain/HomeController.java
+++ src/main/java/kr/or/kosa/visang/domain/HomeController.java
@@ -1,12 +1,16 @@
 package kr.or.kosa.visang.domain;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
   @GetMapping("/")
-  public String home() {
-    return "layout/main";    
+  public String home(HttpServletRequest request, Model model) {
+    String currentUri = request.getRequestURI();
+    model.addAttribute("currentUri", currentUri);
+    return "layout/main";
   }
 }
