6975dbb24f4ac3a1f28a7f7127a164e18727a192
js l <malangtofu507@gmail.com>
2025-05-21 20:46:38 +0900

    feat: Pretendard Variable 폰트 전역 자동 적용 시스템 구현

--- src/main/java/kr/or/kosa/visang/config/WebMvcConfig.java
+++ src/main/java/kr/or/kosa/visang/config/WebMvcConfig.java
@@ -6,6 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Spring MVC 설정 클래스
+ * 
+ * 참고: 폰트 자동 적용은 WebMvcConfigurer를 통해 구현하지 않고,
+ * layout/main.html과 fragments/header.html에 font.css를 include하는 방식을 사용합니다.
+ * 이 방식이 Thymeleaf 템플릿 엔진의 특성과 잘 맞습니다.
+ */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
